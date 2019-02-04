e5899351d9dc7f9279e914d422cb87d81282c99f
nikhilsharma8997 <nikhil.sharma8398@gmail.com>
2019-02-04 16:38:36 +0530

    Improved formatting and naming conventions

--- js/scripts.js
+++ js/scripts.js
@@ -9,61 +9,69 @@
 */
 
 (function($) {
+  // Remove no-js class
+  $("html").removeClass("no-js");
 
-    // Remove no-js class
-    $('html').removeClass('no-js');
+  // Animate to section when nav is clicked
+  $("header a").click(function(e) {
+    // Treat as normal link if no-scroll class
+    if ($(this).hasClass("no-scroll")) return;
 
-    // Animate to section when nav is clicked
-    $('header a').click(function(e) {
+    e.preventDefault();
+    var heading = $(this).attr("href");
+    var scrollDistance = $(heading).offset().top;
 
-        // Treat as normal link if no-scroll class
-        if ($(this).hasClass('no-scroll')) return;
+    $("html, body").animate(
+      {
+        scrollTop: scrollDistance + "px"
+      },
+      Math.abs(window.pageYOffset - $(heading).offset().top) / 1
+    );
 
-        e.preventDefault();
-        var heading = $(this).attr('href');
-        var scrollDistance = $(heading).offset().top;
+    // Hide the menu once clicked if mobile
+    if ($("header").hasClass("active")) {
+      $("header, body").removeClass("active");
+    }
+  });
 
-        $('html, body').animate({
-            scrollTop: scrollDistance + 'px'
-        }, Math.abs(window.pageYOffset - $(heading).offset().top) / 1);
+  // Scroll to top
+  $("#to-top").click(function() {
+    $("html, body").animate(
+      {
+        scrollTop: 0
+      },
+      500
+    );
+  });
 
-        // Hide the menu once clicked if mobile
-        if ($('header').hasClass('active')) {
-            $('header, body').removeClass('active');
-        }
+  // Scroll to first element
+  $("#lead-down span").click(function() {
+    var scrollDistance = $("#lead")
+      .next()
+      .offset().top;
+    $("html, body").animate(
+      {
+        scrollTop: scrollDistance + "px"
+      },
+      500
+    );
+  });
+
+  // Open mobile menu
+  $("#mobile-menu-open").click(function() {
+    $("header, body").addClass("active");
+  });
+
+  // Close mobile menu
+  $("#mobile-menu-close").click(function() {
+    $("header, body").removeClass("active");
+  });
+
+  // Load additional projects
+  $("#view-more-projects").click(function(e) {
+    e.preventDefault();
+    $(this).fadeOut(300, function() {
+      $("#more-projects").fadeIn(300);
     });
-
-    // Scroll to top
-    $('#to-top').click(function() {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 500);
-    });
-
-    // Scroll to first element
-    $('#lead-down span').click(function() {
-        var scrollDistance = $('#lead').next().offset().top;
-        $('html, body').animate({
-            scrollTop: scrollDistance + 'px'
-        }, 500);
-    });
-
-    // Open mobile menu
-    $('#mobile-menu-open').click(function() {
-        $('header, body').addClass('active');
-    });
-
-    // Close mobile menu
-    $('#mobile-menu-close').click(function() {
-        $('header, body').removeClass('active');
-    });
-
-    // Load additional projects
-    $('#view-more-projects').click(function(e){
-        e.preventDefault();
-        $(this).fadeOut(300, function() {
-            $('#more-projects').fadeIn(300);
-        });
-    });
-
+  });
 })(jQuery);
